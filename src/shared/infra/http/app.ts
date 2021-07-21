42904284e0063a09609aaf51bc196870cc880fbf
Raul Neto <raulneto90@gmail.com>
2021-07-21 19:52:04 -0300

    feat: ts-node-dev, ErrorHandler, debug

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,24 @@
+import express, { NextFunction, Request, Response } from 'express';
+
+import { ErrorHandler } from '@shared/errors/ErrorHandler';
+
+import 'dotenv/config';
+
+import { routes } from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof ErrorHandler) {
+      return response.status(error.statusCode).json({ message: error.message });
+    }
+
+    return response.status(500).json({ message: error.message });
+  },
+);
+
+export default app;
