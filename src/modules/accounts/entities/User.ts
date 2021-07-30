efbe0c2be77bd56e25e217c27581b8b7bb9b609c
Raul Neto <raulneto90@gmail.com>
2021-07-30 10:51:19 -0300

    feat: migration e entity de usuarios

---
+++ src/modules/accounts/entities/User.ts
@@ -0,0 +1,39 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity('users')
+export class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  driverLicense: string;
+
+  @Column()
+  isAdmin: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  constructor() {
+    if (!this.id) this.id = uuid();
+  }
+}
