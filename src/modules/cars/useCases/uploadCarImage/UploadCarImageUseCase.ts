5ad9c02234f7037c3a2fa00d4fcc6c19408873c1
Raul Neto <raulneto90@gmail.com>
2021-08-21 15:25:42 -0300

    feat: create car images repository

--- src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
@@ -1,6 +1,15 @@
+import { inject, injectable } from 'tsyringe';
+
 import { ICreateCarImagesDTO } from '@modules/cars/dtos/ICreateCarImageDTO';
 import { CarImage } from '@modules/cars/infra/typeorm/entities/CarImage';
+import { ICarImagesRepository } from '@modules/cars/repositories/ICarImagesRepository';
 
+@injectable()
 export class UploadCarImageUseCase {
+  constructor(
+    @inject('CarImagesRepository')
+    private carImagesRepository: ICarImagesRepository,
+  ) {}
+
   async execute(data: ICreateCarImagesDTO): Promise<CarImage> {}
 }
